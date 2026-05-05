f2e59a1ee6fe2767ac14f449056ea11f1c1c0441
aqibali541k <aqibali541k@gmail.com>
2026-05-05 10:51:53 -0700

    complete ui change

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./App.css";
 import "./loader.css";
 import "@ant-design/v5-patch-for-react-19";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Index from "./pages/routes";
 function App() {
   return (
     <div>
+      <ToastContainer />
       <Index />
     </div>
   );
